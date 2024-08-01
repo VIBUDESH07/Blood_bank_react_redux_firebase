a583b4256a771e400e4bf5396de9bb5c43b21acf
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-08-01 21:03:51 +0530

    Update App.js

--- blood_bank/src/App.js
+++ blood_bank/src/App.js
@@ -10,23 +10,36 @@ import Data from './components/Data';
 import AddData from './components/AddData';
 import FetchData from './components/FetchData';
 import Bbdash from './components/Bbdash';
-
+import ProtectedRoute from './components/ProtectedRoute'; // Import the new ProtectedRoute component
 
 function App() {
   return (
     <>
-      
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/home" element={<Layout />} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/data" element={<Data/>}/>
-        <Route path="/add-data" element={<AddData/>}/>
-        <Route path="/bb-dash" element={<Bbdash/>}/>
-        <Route path="/hos-data" element={<FetchData/>}/>
-        </Routes>
-      
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/data" element={<Data />} />
+        <Route path="/add-data" element={
+          <ProtectedRoute 
+            element={AddData} 
+            requiredType="blood_bank" // Only blood bank users can access AddData
+          />} 
+        />
+        <Route path="/bb-dash" element={
+          <ProtectedRoute 
+            element={Bbdash} 
+            requiredType="blood_bank" // Only blood bank users can access Bbdash
+          />} 
+        />
+        <Route path="/hos-data" element={
+          <ProtectedRoute 
+            element={FetchData} 
+            requiredType="hospital" // Only hospital users can access FetchData
+          />} 
+        />
+      </Routes>
     </>
   );
 }
