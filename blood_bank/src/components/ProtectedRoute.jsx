d2787a2e34afb2de68b60fb02e449c710225869a
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-08-01 21:07:12 +0530

    Update ProtectedRoute.jsx

--- blood_bank/src/components/ProtectedRoute.jsx
+++ blood_bank/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component, userType, requiredType, ...rest }) => {
+const ProtectedRoute = ({ element: Component, userType, requiredType, isAdmin = false, ...rest }) => {
   const isLoggedIn = localStorage.getItem('login') === 'true';
   const storedUserType = localStorage.getItem('userType');
+  const storedIsAdmin = localStorage.getItem('isAdmin') === 'true';
 
-  if (isLoggedIn && storedUserType === requiredType) {
+  if (isLoggedIn && storedUserType === requiredType && (!isAdmin || storedIsAdmin)) {
     return <Component {...rest} />;
   } else {
     return <Navigate to="/login" />;
